18aba7e5bf6a85aab8c0bbbc917b7e9720d33d0e
loganmulvihill <mulvihill.logan@gmail.com>
2020-09-23 22:26:22 -0500

    completed node.js videos through 20

---
+++ Sept-23rd/Video 20/Starter/app.js
@@ -0,0 +1,16 @@
+// Your Javascript Code Goes Here
+
+function Person (firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+}
+
+Person.prototype.greet = function () {
+  console.log('Hello, ' + this.firstName + this.lastName)
+}
+
+var john = new Person('John', 'Doe');
+
+john.greet();
+
+console.log(john.__proto__);
